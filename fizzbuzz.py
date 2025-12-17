fbf502e8f332039cde7fe2bddee0ce312c8d57e0
yoon-chung <yooni0125@gmail.com>
2025-12-17 18:12:16 +0900

    feat: Print fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,9 @@
-for i in range(1, 15+1):
-    if i % 5 == 0:
+for i in range(16, 30+1):
+    if i % 15 == 0:
+        print('fizzbuzz')
+    if i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
         print('buzz')
     else:
         print(i)
